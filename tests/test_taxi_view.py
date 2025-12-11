665b3e86d0ab77b727bff2cc17c87f3ecc6d66ec
Andrii Chychkan <mywork1728@gmail>
2025-12-11 12:20:01 +0200

    fixed

--- tests/test_taxi_view.py
+++ tests/test_taxi_view.py
@@ -71,11 +71,11 @@ class DriverListTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "taxi/driver_list.html")
 
-    def test_car_list_paginated_correctly(self):
+    def test_driver_list_paginated_correctly(self):
         response = self.client.get(DRIVER_LIST_URL)
         self.assertEqual(len(response.context["drivers"]), PAGINATION)
 
-    def test_car_detail_response_with_correct_template(self):
+    def test_driver_detail_response_with_correct_template(self):
         response = self.client.get(reverse("taxi:driver-detail", args=[1]))
 
         self.assertEqual(response.status_code, 200)
